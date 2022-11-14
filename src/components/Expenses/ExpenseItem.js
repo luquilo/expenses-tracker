66e6696fbaf7a1cdfbe566d0bee908f0b74f24d6
luquilo <luquilo.etc@gmail.com>
2022-11-14 13:25:05 +0700

    learning about state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -20,7 +20,7 @@ const ExpenseItem = (props) => {
         <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>marks as read</button>
+      <button onClick={clickHandler}>click me!</button>
     </Card>
   );
 };
