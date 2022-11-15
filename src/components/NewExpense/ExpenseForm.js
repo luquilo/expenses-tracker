12cdf9e63e4f81cc78ce4a8ef8b405fbc4ac1977
luquilo <luquilo.etc@gmail.com>
2022-11-15 17:42:21 +0700

    creating form component

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,28 @@
+import React from "react";
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+    return (
+        <form>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label htmlFor="">Title</label>
+                    <input type="text" name="" id="" />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="">Amount</label>
+                    <input type="number" min="0.01" step="0.01" name="" id="" />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="">Date</label>
+                    <input type="date" name="" id="" min="2019-01-01" max="2022-12-31"/>
+                </div>
+            </div>
+            <div className="new-expense__actions">
+                <button>Add Expense</button>
+            </div>
+        </form>
+    )
+}
+
+export default ExpenseForm;
